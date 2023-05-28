fbebfd932224428bf620417588d16959e9ad4722
vishal <vg.13061999@gmail.com>
2023-05-28 11:56:28 +0530

    doremon added

---
+++ doremon.v
@@ -0,0 +1,6 @@
+doremon is a good cartoon 
+i used to watch that cartoon in my childhood
+but there is one reason behind why i started to like that cartoon, because i brother love to watch cartoon and i am fond of watching CI
+d
+[A[A[B[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[CD and addalat but because of him i a[D[D[B[Aalso started to watch that cartoon and i also strted to like that cartoon serial 
+that's it only.
